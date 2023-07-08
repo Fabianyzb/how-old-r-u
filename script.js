51a5349d3d8ad6436e38660bedc3d66b02cf18e1
Fabian Yanez <fabian.yzb@gmail.com>
2023-07-08 00:48:51 -0400

    feat

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dateInput = document.querySelector('input');
 const pElement = document.querySelector('p');
+const myImage = document.querySelector('img');
 
 dateInput.addEventListener('change', () => {
     const birthDate = new Date(dateInput.value);
@@ -11,4 +12,15 @@ dateInput.addEventListener('change', () => {
     const days = newDate.getDate() - 1;
 
     pElement.innerText = `You're ${years} years, ${months} months, ${days} days old!`;
-})
+
+    if (years >= 6) {
+        myImage.src = '/img/jojo.gif'; // Cambia 'ruta_de_la_imagen.png' por la ruta de la imagen que deseas mostrar
+        myImage.alt = 'Imagen activada'; // Cambia 'Imagen activada' por el texto alternativo de la imagen activada
+    } else {
+        myImage.src = '/img/question.png'; // Ruta de la imagen original
+        myImage.alt = 'Imagen original'; // Texto alternativo de la imagen original
+    }
+}) 
+
+/img/okanim.png
+/img/question.png
